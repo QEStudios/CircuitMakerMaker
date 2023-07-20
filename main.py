ef8836eccdac82405aa27512ff204a29875befde
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-07-20 21:59:14 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,122 @@
+from render import render
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import re
+import requests
+import time
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print(f'Bot ID: {bot.user.id}')
+    print('-----')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    linkRegex = r"(https?:\/\/(www\.)?(dpaste\.org/([-a-zA-Z0-9]*)\/raw|pastebin\.com\/raw\/([-a-zA-Z0-9]*)))"
+    saveRegex = (
+    # Match all blocks
+    r"(((\d+,){2}(-?\d+(\.\d+)?,){3}(((\d+(\.\d+)?\+)*(\d+(\.\d+)?)))?;)*"
+    r"((\d+,){2}(-?\d+(\.\d+)?,){3}(((\d+(\.\d+)?\+)*(\d+(\.\d+)?)))?\?)"
+    # Match all connections
+    r"((([1-9][0-9]*),([1-9][0-9]*)|((([1-9][0-9]*),([1-9][0-9]*);)+"
+    r"([1-9][0-9]*),([1-9][0-9]*)))?\?)"
+    # Match custom build syntax
+    r"((\w+(,(-?\d+(\.\d+)?(\+-?\d+(\.\d+)?)*)*)+)(;(\w+(,(-?\d+(\.\d+)?(\+-?\d+(\.\d+)?)*)*)+))*)*\?"
+    # Match sign data
+    r"[0-9a-f]*(;[0-9a-fA-F]*)*)"
+)
+    if re.search(linkRegex, message.content):
+        totalStart = time.time()
+        try:
+            url = re.search(linkRegex, message.content).group(0)
+            saveString = requests.get(url).text
+            renderStart = time.time()
+            renderedImage, save = render(saveString)
+            renderTime = round((time.time() - renderStart) * 1000, 1)
+            previewFile = discord.File(fp=renderedImage, filename="preview.png")
+            embed = discord.Embed(title="Save info")
+            embed.add_field(name="Blocks", value=str(len(save.blocks)), inline=True)
+            embed.add_field(name="Connections", value="\"ill add this later\" - skm", inline=True)
+            embed.add_field(name="Raw size", value=str(len(saveString)), inline=True)
+            embed.set_image(url="attachment://preview.png")
+
+            totalTime = round((time.time() - totalStart) * 1000, 1)
+            embed.set_footer(text=f"Preview took {renderTime} ms to render, total response time {totalTime} ms.")
+
+            await message.reply("Here's a preview of that save!", file=previewFile, embed=embed, mention_author=False)
+            renderedImage.close()
+        except Exception as e:
+            await message.reply(f"Sorry, I couldn't render a preview for that save! Here's the error: {e}")
+    elif re.search(saveRegex, message.content):
+        totalStart = time.time()
+        saveString = re.search(saveRegex, message.content).group(0)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        payload = {"lexer": "_text", "format": "url", "content": saveString}
+        try:
+            res = requests.post("https://dpaste.org/api/", headers=headers, data=payload)
+            res.raise_for_status()
+            url = res.text.rstrip("\n") + "/raw"
+
+            renderStart = time.time()
+            renderedImage, save = render(saveString)
+            renderTime = round((time.time() - renderStart) * 1000, 1)
+            previewFile = discord.File(fp=renderedImage, filename="preview.png")
+            embed = discord.Embed(title="Save info")
+            embed.add_field(name="Blocks", value=str(len(save.blocks)), inline=True)
+            embed.add_field(name="Connections", value="\"ill add this later\" - skm", inline=True)
+            embed.add_field(name="Raw size", value=str(len(saveString)), inline=True)
+            embed.set_image(url="attachment://preview.png")
+
+            totalTime = round((time.time() - totalStart) * 1000, 1)
+            embed.set_footer(text=f"Preview took {renderTime} ms to render, total response time {totalTime} ms.")
+
+            await message.reply(f"Here's a dpaste link for that save: {url}\nAnd here's a preview of that save, too!", file=previewFile, embed=embed, mention_author=False)
+        except Exception as e:
+            print(f"An error occured while uploading to dpaste: {e}")
+            await message.reply(f"Sorry, I couldn't render a preview for that save! Here's the error: {e}")
+    elif len(message.attachments) > 0:
+        fileBytes = await message.attachments[0].read()
+        fileString = fileBytes.decode()
+        if re.match(saveRegex, fileString):
+            totalStart = time.time()
+            saveString = fileString
+            headers = {"User-Agent": "Mozilla/5.0"}
+            payload = {"lexer": "_text", "format": "url", "content": saveString}
+            try:
+                res = requests.post("https://dpaste.org/api/", headers=headers, data=payload)
+                res.raise_for_status()
+                url = res.text.rstrip("\n") + "/raw"
+
+                renderStart = time.time()
+                renderedImage, save = render(saveString)
+                renderTime = round((time.time() - renderStart) * 1000, 1)
+                previewFile = discord.File(fp=renderedImage, filename="preview.png")
+                embed = discord.Embed(title="Save info")
+                embed.add_field(name="Blocks", value=str(len(save.blocks)), inline=True)
+                embed.add_field(name="Connections", value="\"ill add this later\" - skm", inline=True)
+                embed.add_field(name="Raw size", value=str(len(saveString)), inline=True)
+                embed.set_image(url="attachment://preview.png")
+
+                totalTime = round((time.time() - totalStart) * 1000, 1)
+                embed.set_footer(text=f"Preview took {renderTime} ms to render, total response time {totalTime} ms.")
+
+                await message.reply(f"Here's a dpaste link for that save: {url}\nAnd here's a preview of that save, too!", file=previewFile, embed=embed, mention_author=False)
+            except Exception as e:
+                print(f"An error occured while uploading to dpaste: {e}")
+                await message.reply(f"Sorry, I couldn't render a preview for that save! Here's the error: {e}")
+
+bot.run(TOKEN)
---
+++ render.py
@@ -0,0 +1,112 @@
+import cm2py as cm2
+import numpy as np
+import math
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+def render(saveString):
+    save = cm2.importSave(saveString)
+
+    size = (1600, 1200)
+    angle = [0,0]
+
+    blockColours = [
+        (255, 9, 0),
+        (0, 121, 255),
+        (0, 241, 29),
+        (168, 0, 255),
+        (255, 127, 0),
+        (30, 30, 30),
+        (175, 175, 175),
+        (175, 131, 76),
+        (73, 185, 255),
+        (255, 72, 72),
+        (0, 42, 89),
+        (213, 0, 103),
+        (84, 54, 35),
+        (25, 71, 84),
+    ]
+
+    sqrt3 = math.sqrt(3)
+    sqrt2 = math.sqrt(2)
+    def project(points):
+        if angle[1] == 0:
+            matrix = np.array([
+                [sqrt3, 0, -sqrt3],
+                [1, 2, 1],
+                [sqrt2, -sqrt2, sqrt2]], dtype=float)
+        projected = np.dot(matrix, points.T).T
+        projected[:, 1] *= -1
+        return projected
+
+    def drawBlock(b, p):
+        x = p[0]*scale + size[0]/2 - bounds[0][0]*scale - sizeX/2*scale
+        y = p[1]*scale + size[1]/2 - bounds[1][0]*scale - sizeY/2*scale
+        if angle[1] == 0:
+            draw.polygon([
+                (x-sqrt3*scale,y-scale),
+                (x,y-2*scale),
+                (x+sqrt3*scale,y-scale),
+                (x+sqrt3*scale,y+scale),
+                (x,y+2*scale),
+                (x-sqrt3*scale,y+scale)], fill=blockColours[b.blockId])
+            draw.line([
+                (x-sqrt3*scale,y-scale),
+                (x,y-2*scale),
+                (x+sqrt3*scale,y-scale),
+                (x+sqrt3*scale,y+scale),
+                (x,y+2*scale),
+                (x-sqrt3*scale,y+scale),
+                (x-sqrt3*scale,y-scale),
+                (x,y),
+                (x+sqrt3*scale,y-scale)], fill=0, width=int(scale/6), joint="curve")
+            draw.line([
+                (x,y),
+                (x,y+2*scale)], fill=0, width=int(scale/6), joint="curve")
+
+    positions = [(b.x, b.y, 0-b.z) for b in save.blocks]
+    points = np.array(positions)
+    interPoints = project(points)
+    pointIndices = np.flip(np.argsort(interPoints[:, 2]))
+    projectedPoints = interPoints[pointIndices]
+    sortedBlocks = np.array(save.blocks)[pointIndices]
+
+    bounds = [[projectedPoints[0][0], projectedPoints[0][0]], [projectedPoints[0][1], projectedPoints[0][1]]]
+
+    for p in projectedPoints:
+        if p[0] < bounds[0][0]:
+            bounds[0][0] = p[0]
+        if p[0] > bounds[0][1]:
+            bounds[0][1] = p[0]
+        
+        if p[1] < bounds[1][0]:
+            bounds[1][0] = p[1]
+        if p[1] > bounds[1][1]:
+            bounds[1][1] = p[1]
+
+    bounds[0][0] -= 3
+    bounds[0][1] += 3
+    bounds[1][0] -= 3
+    bounds[1][1] += 3
+
+    sizeX = bounds[0][1] - bounds[0][0]
+    sizeY = bounds[1][1] - bounds[1][0]
+
+    scaleX = int(size[0] / sizeX)
+    scaleY = int(size[1] / sizeY)
+
+    scale = min(scaleX, scaleY)
+
+
+    im = Image.new("RGB", (size[0], size[1]), color=(255,255,255))
+    draw = ImageDraw.Draw(im)
+
+    for i in range(len(projectedPoints)):
+        b = sortedBlocks[i]
+        p = projectedPoints[i]
+        drawBlock(b, p)
+
+    stream = BytesIO()
+    im.save(stream, "PNG")
+    stream.seek(0)
+    return stream, save

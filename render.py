dedf48fd2ed9865ff46f0fc8f759b1aac463628d
SKM GEEK <qestudios17@gmail.com>
2023-07-21 19:09:25 +1000

    idk

--- render.py
+++ render.py
@@ -54,7 +54,7 @@ async def render(saveString):
             blockColour = tuple([int(v) for v in b.properties])
         x = p[0]*scale + size[0]/2 - bounds[0][0]*scale - sizeX/2*scale
         y = p[1]*scale + size[1]/2 - bounds[1][0]*scale - sizeY/2*scale
-        pCube = projectedCube + np.repeat(np.array([x,y]),[1,7],axis=0)
+        pCube = projectedCube[0,2] + np.repeat(np.array([[x,y]]),[1,7],axis=0)
         draw.polygon([
             pCube[0],
             pCube[4],

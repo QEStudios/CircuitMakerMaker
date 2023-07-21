6a80d8ef88932ea2860a7909d5d8a417491f2210
SKM GEEK <qestudios17@gmail.com>
2023-07-21 19:56:03 +1000

    ok itll work now <3

--- render.py
+++ render.py
@@ -77,18 +77,18 @@ async def render(saveString):
             pCube[6]], fill=tuple([int(v*.75) for v in blockColour]))
 
         draw.line([
+            pCube[4],
             pCube[0],
             pCube[2],
             pCube[3],
             pCube[7],
             pCube[5],
             pCube[4],
-            pCube[0],
             pCube[6],
             pCube[7]], fill=0, width=int(scale/8), joint="curve")
         draw.line([
-            pCube[6],
-            pCube[4]], fill=0, width=int(scale/8), joint="curve")
+            pCube[2],
+            pCube[6]], fill=0, width=int(scale/8), joint="curve")
 
     positions = [(b.x, b.y, 0-b.z) for b in save.blocks]
     points = np.array(positions)

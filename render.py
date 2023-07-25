4cc8866e632f3cc692cd72a886031c093f817131
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-07-25 19:13:13 +1000

    yeahhh

--- render.py
+++ render.py
@@ -5,6 +5,7 @@ from io import BytesIO
 from PIL import Image, ImageDraw
 import random
 import asyncio
+from transparentGifWorkaround import save_transparent_gif
 
 
 async def render(saveString):
@@ -194,6 +195,6 @@ async def render(saveString):
         frames.append(im)
 
     stream = BytesIO()
-    frames[0].save(stream, "GIF", save_all=True, append_images=frames[1:], optimize=True, duration=1.5, loop=0)
+    save_transparent_gif(images=frames,save_file=stream, durations=1.5)
     stream.seek(0)
     return stream, save

d2816508a359272b10a4f7a7e916f3b7e7960fdf
SKM GEEK <qestudios17@gmail.com>
2023-07-31 21:16:27 +1000

    ctrl+c > ctrl+v > instant profit

--- transparentGifWorkaround.py
+++ transparentGifWorkaround.py
@@ -1,31 +1,28 @@
 from PIL.Image import Image
-import numpy as np
 
 from collections.abc import Collection
-from functools import partial
 from io import BufferedIOBase
 
 
-def process_image(image, alpha_threshold=0) -> Image:
-    image_w_palette = image.convert("P", colors=256-1)
+def process_image(image: Image) -> Image:
+    image_w_palette = image.quantize()
 
-    palette_indices = np.frombuffer(bytearray(image_w_palette.tobytes()), "uint8")
+    palette = image_w_palette.palette.colors
+    transparent_color = min(palette, key=lambda x: x[3])
+    if transparent_color[3] == 255:
+        # don't set a transparent color if there are no transparent colors
+        return image_w_palette
 
-    image_data = np.array(image)
-    palette_indices[image_data.reshape(-1, 4)[:, 3] <= alpha_threshold] = 0xff
+    transparent_index = palette[transparent_color]
 
-    image_w_palette.frombytes(palette_indices.tobytes())
-    image_w_palette.info["transparency"] = 0xff
-    image_w_palette.info["background"] = 0xff
+    image_w_palette.info["transparency"] = transparent_index
+    image_w_palette.info["background"] = transparent_index
 
     return image_w_palette
 
 
-def save_transparent_gif(images: Collection[Image], durations, save_file, alpha_threshold=0):
-    first_image, *remaining_images = map(
-        partial(process_image, alpha_threshold=alpha_threshold),
-        images
-    )
+def save_transparent_gif(images: Collection[Image], durations: int | Collection[int], save_file: BufferedIOBase):
+    first_image, *remaining_images = map(process_image, images)
 
     first_image.save(
         save_file,

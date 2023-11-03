5aa76fa67a5cf980ff7ac81c9f066376e09294c8
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-11-03 15:02:57 +1100

    asdf

--- render.py
+++ render.py
@@ -28,8 +28,8 @@ async def render(saveString, messageId):
 
     def generateText(text):
         fnt = ImageFont.truetype('SourceCodePro-Medium.ttf', 72)
-        size, offset = fnt.font.getsize(text)
-        img = Image.new('RGBA', size, (0,0,0,255))
+        size, offset = fnt.getmask(text).getbbox()
+        img = Image.new('RGBA', size[0], (0,0,0,255))
         thisDraw = ImageDraw.Draw(img)
         thisDraw.text((0,0), text, (255, 255, 255), font=fnt)
         return img

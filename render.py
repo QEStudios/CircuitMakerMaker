cfe52547f4aa495f50f29990d56c2f084fdaf25a
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-11-03 13:52:54 +1100

    a

--- render.py
+++ render.py
@@ -189,7 +189,7 @@ async def render(saveString, messageId):
             p = projectedPoints[i]
             drawBlock(b, p)
     
-        im.mode = "RGBA"
+        im.convert("RGBA")
         frames.append(im)
 
     outputFilename = f"result{messageId}.gif"

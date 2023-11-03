d260fc474086dc11c3aa1460208195c9ce84691e
SKM GEEK <qestudios17@gmail.com>
2023-11-04 01:13:05 +1100

    int

--- imageGen.py
+++ imageGen.py
@@ -5,7 +5,7 @@ from PIL import Image
 def image(im: Image.Image, size):
     largestDim = max(im.width, im.height)
     scale = size/largestDim
-    dim = (im.width*scale, im.height*scale)
+    dim = (int(im.width*scale), int(im.height*scale))
     im = im.resize(dim)
     pixels = list(im.getdata())
 

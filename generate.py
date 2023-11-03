86fbac82ddb391870a3008ffa304de0fec03c219
SKM GEEK <qestudios17@gmail.com>
2023-11-03 19:00:00 +1100

    Hopefully this works now

--- generate.py
+++ generate.py
@@ -1,19 +1,19 @@
-import generate.clock
-import generate.counter
+import clockGen
+import counterGen
 import math
 
 def clock(period):
     if period <= 5:
-        return generate.clock.chain(period)
+        return clockGen.chain(period)
     else:
-        return generate.clock.cycle(period)
+        return clockGen.cycle(period)
 
 def counter(minVal, maxVal, direction):
     if minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == 1:
-        return generate.counter.standard(int(math.log2(maxVal+1)))
+        return counterGen.standard(int(math.log2(maxVal+1)))
     elif minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == -1:
-        return generate.counter.reverse(int(math.log2(maxVal+1)))
+        return counterGen.reverse(int(math.log2(maxVal+1)))
     elif minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == -1:
-        return generate.counter.both(int(math.log2(maxVal+1)))
+        return counterGen.both(int(math.log2(maxVal+1)))
     else:
         return

de3f6ad8f30c7507cdc1676e01913c4840d40e2a
SKM GEEK <qestudios17@gmail.com>
2024-07-16 23:33:58 +1000

    fix missing arguments in generate.py

--- generate.py
+++ generate.py
@@ -4,25 +4,28 @@ import imageGen
 
 import math
 
+
 def clock(period):
     if period <= 5:
         return clockGen.chain(period)
     else:
         return clockGen.cycle(period)
 
+
 def counter(minVal, maxVal, direction):
-    if minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == 1:
-        return counterGen.standard(math.ceil(math.log2(maxVal+1)))
-    elif minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == -1:
-        return counterGen.reverse(math.ceil(math.log2(maxVal+1)))
-    elif minVal == 0 and (math.log2(maxVal+1))%1 == 0 and direction == 0:
-        return counterGen.both(math.ceil(math.log2(maxVal+1)))
+    if minVal == 0 and (math.log2(maxVal + 1)) % 1 == 0 and direction == 1:
+        return counterGen.standard(math.ceil(math.log2(maxVal + 1)))
+    elif minVal == 0 and (math.log2(maxVal + 1)) % 1 == 0 and direction == -1:
+        return counterGen.reverse(math.ceil(math.log2(maxVal + 1)))
+    elif minVal == 0 and (math.log2(maxVal + 1)) % 1 == 0 and direction == 0:
+        return counterGen.both(math.ceil(math.log2(maxVal + 1)))
     elif direction == 1:
-        return counterGen.standard(math.ceil(math.log2(maxVal+1)), (minVal,maxVal))
+        return counterGen.standard(math.ceil(math.log2(maxVal + 1)), (minVal, maxVal))
     elif direction == -1:
-        return counterGen.reverse(math.ceil(math.log2(maxVal+1)), (minVal,maxVal))
+        return counterGen.reverse(math.ceil(math.log2(maxVal + 1)), (minVal, maxVal))
     else:
         return
 
-def image(im, scale):
-    return imageGen.image(im, scale)
+
+def image(im, scale, transparency):
+    return imageGen.image(im, scale, transparency)

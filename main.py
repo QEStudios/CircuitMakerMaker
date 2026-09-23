c1b4b6590541249165f74ee5808614ae2d7bcd80
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2026-09-24 05:07:46 +1000

    Lower Kogge-Stone max size from 8192 to 256

--- main.py
+++ main.py
@@ -382,7 +382,7 @@ async def counter(ctx, min: int, max: int, direction: str):
 @option("bits",
     description="Size of the adder.",
     min_value=1,
-    max_value=8192,
+    max_value=256,
     default=8,
     required=True
 )
